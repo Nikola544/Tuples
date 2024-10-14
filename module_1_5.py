7902c63f93f72c6c7ab753b7904394682c0be8cf
Николай <dhyeheydddjeujd>
2024-10-14 16:41:35 +0300

    14/10

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = (1, 2, 'a', 'b',)
 print( immutable_var)
-mutable_list = ( [1, 2, 'a', 'b',] )
+mutable_list = ( [1, 2, ('a', 'b')] )
 print(mutable_list)
 mutable_list[0] = 2
 print(mutable_list)
